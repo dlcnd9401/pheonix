84f99ef20bfa4af76ea211b69153e17b1d91c2b1
GD <GD@DESKTOP-TL3UJ6Q>
2017-10-18 11:09:24 +0900

    dao,service

---
+++ src/main/java/kr/gudi/phoenix/dao/Testdao.java
@@ -0,0 +1,19 @@
+package kr.gudi.phoenix.dao;
+
+import java.util.HashMap;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Testdao implements TestdaoInterface {
+	@Resource(name="sqlSession")
+	SqlSession session;
+	
+	@Override
+	public HashMap<String, Object> test() {
+		return session.selectOne("sql.test");
+	}
+}
---
+++ src/main/java/kr/gudi/phoenix/dao/TestdaoInterface.java
@@ -0,0 +1,9 @@
+package kr.gudi.phoenix.dao;
+
+import java.util.HashMap;
+
+public interface TestdaoInterface {
+
+	public HashMap<String, Object> test();
+
+}
