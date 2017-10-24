0e33a4f82487407a953b7496b1c3fbc2b4683233
GD <GD@DESKTOP-V518NHV>
2017-10-24 11:29:17 +0900

    fdsfds

--- src/main/java/kr/gudi/phoenix/dao/QuestionDaoInterface.java
+++ src/main/java/kr/gudi/phoenix/dao/QuestionDaoInterface.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface QuestionDaoInterface {
 	public List<HashMap<String, Object>> getData();
 	public HashMap<String, Object> getDetailData(HashMap<String, Object> param);
+	public int setWriteData(HashMap<String, Object> param); //값을 넣는거니까 ->set 
 }
